c7dcba23f508d31fde33c29e54dd40ea84fd5b78
DiegoFreo <diego_freo8@hotmail.com>
2026-06-08 22:10:39 -0300

    feat: implementacao do sistema de cronometragem rfid e relatorios

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // Ignora erros de tipagem estrita apenas no build de produção da Vercel
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Ignora avisos do linter na hora do build
-    ignoreDuringBuilds: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
